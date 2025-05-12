1110b9f8503307002c954977a4342a190c33044e
Luis Palominos <lpalominosg@gmail.com>
2025-05-12 13:52:28 -0400

    Updating responsive

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const letters = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ".split("");
 let countdown;
 let time = 10;
@@ -32,44 +33,3 @@ window.onload = () => {
     container.appendChild(button);
   });
 };
-
-function startRound() {
-  const categories = ["Nombre MASCULINO", "País del mundo", "Animal", "Algo que VUELE", "Color", "Comida"];
-  const random = categories[Math.floor(Math.random() * categories.length)];
-  document.getElementById("category").innerText = random;
-}
-
-function resetTimer() {
-  document.getElementById("click-sound").play();
-  clearInterval(countdown);
-  time = 10;
-  document.getElementById("timer").innerText = time;
-
-  setProgress(0);
-  let progress = 0;
-
-  countdown = setInterval(() => {
-    time--;
-    progress += 10;
-    document.getElementById("timer").innerText = time;
-    setProgress(progress);
-
-    if (time <= 0) {
-	  document.getElementById("alarm-sound").play();
-      clearInterval(countdown);
-      setProgress(100);
-    }
-  }, 1000);
-}
-
-// Temporizador visual
-const circle = document.querySelector(".progress-ring__circle");
-const radius = circle.r.baseVal.value;
-const circumference = 2 * Math.PI * radius;
-circle.style.strokeDasharray = `${circumference} ${circumference}`;
-circle.style.strokeDashoffset = circumference;
-
-function setProgress(percent) {
-  const offset = circumference - (percent / 100) * circumference;
-  circle.style.strokeDashoffset = offset;
-}
